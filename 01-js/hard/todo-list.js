99a83f2d592083ded3d7f78b9500d8e8a2cae1cc
sanketmunot <sanketmunot95@gmail.com>
2023-07-05 22:18:02 +0530

    todo solution

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,35 @@
 */
 
 class Todo {
+  constructor(){
+    this.todos = [];
+  }
 
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  remove(idx) {
+    this.todos.splice(idx,1);
+  }
+
+  update(idx, newValue) {
+    if (this.todos[idx]){
+      this.todos[idx] = newValue;
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(idx) {
+    return this.todos[idx] || null;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
