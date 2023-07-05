a4044b555bbf71bdf92b8cc503a188f684242e4f
sanketmunot <sanketmunot95@gmail.com>
2023-07-05 18:57:24 +0530

    anagram solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,36 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase()
+  str2=str2.toLowerCase()
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let map1 = {},
+    map2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (map1[str1[i]]) {
+      map1[str1[i]] += 1;
+    } else {
+      map1[str1[i]] = 1;
+    }
 
+    if (map2[str2[i]]) {
+      map2[str2[i]] += 1;
+    } else {
+      map2[str2[i]] = 1;
+    }
+  }
+
+  let res = true;
+  Object.keys(map1).forEach(i => {
+    if(map1[i] != map2[i]){
+      res = false
+      return
+    }
+  });
+
+  return res;
 }
 
 module.exports = isAnagram;
